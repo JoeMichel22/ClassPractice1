5de856aa2d944828cafa47518ded0ec04623641e
Joe Michel <tun69277@temple.edu>
2023-03-28 12:21:40 -0400

    Timer tied to button click logic

--- app/src/main/java/com/example/classpractice1/MainActivity.kt
+++ app/src/main/java/com/example/classpractice1/MainActivity.kt
@@ -3,15 +3,35 @@ package com.example.classpractice1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    val timerView: TextView by lazy{
+        findViewById(R.id.tvTimer)
+    }
+    val countBTN: Button by lazy{
+        findViewById(R.id.btnCount)
+    }
+
+    val scope= CoroutineScope(Job() + Dispatchers.Default)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Thread{
-            repeat(100){
-                Log.d("Countdown", (100 - it).toString())
+        countBTN.setOnClickListener {
+            scope.launch {
+                repeat(100) {
+                    (100 - it).toString().run {
+                        Log.d("Countdown", this)
+                        withContext(Dispatchers.Main){
+                             timerView.text= this@run
+                        }
+                    }
+                    delay(500)
+                }
             }
         }
     }
